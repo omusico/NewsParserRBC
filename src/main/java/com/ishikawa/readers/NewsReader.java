7ed9d59c69ccafeeed71c95e69b323e9464142ec
ishikawayoshi <Ishikawa17011985>
2013-10-24 18:32:03 +0400

    Add two empty classes

---
+++ src/main/java/com/ishikawa/readers/NewsReader.java
@@ -0,0 +1,15 @@
+package com.ishikawa.readers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ishikawa
+ * Date: 24.10.13
+ * Time: 18:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewsReader implements NewsSites {
+    public static void main(String[] args) {
+        System.out.println(RBC_RU);
+    }
+
+}
---
+++ src/main/java/com/ishikawa/readers/NewsSites.java
@@ -0,0 +1,12 @@
+package com.ishikawa.readers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ishikawa
+ * Date: 24.10.13
+ * Time: 18:27
+ * To change this template use File | Settings | File Templates.
+ */
+public interface NewsSites {
+    public static final String RBC_RU = "http://rbc.ru";
+}

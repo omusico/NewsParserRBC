bd599c68dc5ac75332b208a9193952ec9983b9c2
ishikawayoshi <Ishikawa17011985>
2013-10-24 20:02:49 +0400

    Additional Add

--- src/main/java/com/ishikawa/readers/NewsReader.java
+++ src/main/java/com/ishikawa/readers/NewsReader.java
@@ -8,13 +8,7 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.IOException;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Ishikawa
- * Date: 24.10.13
- * Time: 18:26
- * To change this template use File | Settings | File Templates.
- */
+
 public class NewsReader implements NewsSites {
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.parse(new File("C:\\javaProjects\\incubator\\NewsParserRBC\\news_sites_ru.html"), "UTF-8");

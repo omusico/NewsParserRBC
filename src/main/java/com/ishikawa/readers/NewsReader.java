a6396892721157b01f54c2697b782a97f272657a
ishikawayoshi <Ishikawa17011985>
2013-10-24 19:35:44 +0400

    add list of russian news sites + NewsRader minor inprogress changes

--- src/main/java/com/ishikawa/readers/NewsReader.java
+++ src/main/java/com/ishikawa/readers/NewsReader.java
@@ -1,5 +1,13 @@
 package com.ishikawa.readers;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Ishikawa
@@ -8,8 +16,19 @@ package com.ishikawa.readers;
  * To change this template use File | Settings | File Templates.
  */
 public class NewsReader implements NewsSites {
-    public static void main(String[] args) {
-        System.out.println(RBC_RU);
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.parse(new File("C:\\javaProjects\\incubator\\NewsParserRBC\\news_sites_ru.html"), "UTF-8");
+
+        Elements links = doc.getElementsByTag("a");
+        System.out.println(links.size());
+
+
+        for (Element link : links) {
+            String linkHref = link.attr("href");
+            String linkText = link.text();
+            System.out.println(linkText + "  " + linkHref);
+        }
+
     }
 
 }
